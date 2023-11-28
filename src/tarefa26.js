cd93901791bfb115a20632452176ac3ebeb82790
CarlosMontealto <kadu_1401@hotmail.com>
2023-11-28 08:14:36 -0300

    adição do exercicio_ts

---
+++ src/tarefa26.js
@@ -0,0 +1,12 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log("Multiplicação de 13x27");
+console.log(multiplicacao(13, 27));
+console.log("");
+console.log("Saudação para Mario");
+console.log(saudacao("Mario"));
---
+++ src/tarefa26.ts
@@ -0,0 +1,14 @@
+function multiplicacao(a: number, b: number): number {
+  return a * b;
+}
+
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+console.log("Multiplicação de 13x27");
+console.log(multiplicacao(13, 27));
+console.log("");
+console.log("Saudação para Mario");
+console.log(saudacao("Mario"));
